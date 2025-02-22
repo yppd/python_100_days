4bc8780d311e58fd05a9cc21c3016ba2909eab36
Yannis <yppd@outlook.com>
2025-02-22 16:24:29 +0200

    corrected branch

---
+++ Day1/task.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+print("Welcome to the Band Name Generator.\n")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be: " + city_name + " " + pet_name)
