2849ef1716d1fb5f020da145e89bb8ba82c3101e
Yannis Papadimitriou <yannis.papadimitriou@cegeka.com>
2025-02-25 13:06:18 +0200

    Day 4 rock paper scissors project

---
+++ Day04/solution.py
@@ -0,0 +1,8 @@
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+random_num = random.randint(0,4)
+
+print(friends[random_num])
+
+print(random.choice(friends))
---
+++ Day04/task.py
@@ -0,0 +1,47 @@
+# import random
+
+
+# # random_integer = random.randint(1,10)
+# # print(random_integer)
+
+# # random_number_0_to_1 = random.random() *10
+# # print(random_number_0_to_1)
+
+# # random_floating_number = random.uniform(1, 10)
+# # print(random_floating_number)
+
+
+# # tosh = random.randint(0, 1)
+
+# # if tosh == 0:
+# #     print("Heads")
+# # else:
+# #     print("Tails")
+
+
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut",
+#         "Massachusetts", "Maryland", "South Carolina", "New Hampshire", 
+#         "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", 
+#         "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", 
+#         "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", 
+#         "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", 
+#         "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", 
+#         "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", 
+#         "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+# print(states_of_america[50])
+# print(len(states_of_america))
+
+
+
+
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears",
+# "Tomatoes", "Celery", "Potatoes"]
+
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
