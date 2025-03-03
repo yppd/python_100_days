d87afef711f4c067d9bf0ffb804952c640d21ad6
Yannis <yppd@outlook.com>
2025-03-03 11:35:15 +0200

    cleaned up code

--- Day05/password_generator.py
+++ Day05/password_generator.py
@@ -1,5 +1,6 @@
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -26,18 +27,16 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
 password = []
-for letter in range(0,nr_letters ):
+for letter in range(0, nr_letters):
     password.append(random.choice(letters))
 
-for symbol in range(0,nr_symbols):
+for symbol in range(0, nr_symbols):
     password.append(random.choice(symbols))
 
-for number in range(0,nr_numbers):
+for number in range(0, nr_numbers):
     password.append(random.choice(numbers))
 
-print(password)
 random.shuffle(password)
-print(password)
 
 my_password = ''
 for char in password:
