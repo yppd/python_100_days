a9e5fc161a791a2eff839b5af41408757d134841
Yannis Papadimitriou <yannis.papadimitriou@cegeka.com>
2025-03-11 16:31:10 +0200

    caesar cipher final

---
+++ Day08/art.py
@@ -0,0 +1,39 @@
+logo = """           
+                                                                                                               
+    cccccccccccccccc  aaaaaaaaaaaaa      eeeeeeeeeeee        ssssssssss     aaaaaaaaaaaaa  rrrrr   rrrrrrrrr   
+  cc:::::::::::::::c  a::::::::::::a   ee::::::::::::ee    ss::::::::::s    a::::::::::::a r::::rrr:::::::::r  
+ c:::::::::::::::::c  aaaaaaaaa:::::a e::::::eeeee:::::eess:::::::::::::s   aaaaaaaaa:::::ar:::::::::::::::::r 
+c:::::::cccccc:::::c           a::::ae::::::e     e:::::es::::::ssss:::::s           a::::arr::::::rrrrr::::::r
+c::::::c     ccccccc    aaaaaaa:::::ae:::::::eeeee::::::e s:::::s  ssssss     aaaaaaa:::::a r:::::r     r:::::r
+c:::::c               aa::::::::::::ae:::::::::::::::::e    s::::::s        aa::::::::::::a r:::::r     rrrrrrr
+c:::::c              a::::aaaa::::::ae::::::eeeeeeeeeee        s::::::s    a::::aaaa::::::a r:::::r            
+c::::::c     ccccccca::::a    a:::::ae:::::::e           ssssss   s:::::s a::::a    a:::::a r:::::r            
+c:::::::cccccc:::::ca::::a    a:::::ae::::::::e          s:::::ssss::::::sa::::a    a:::::a r:::::r            
+ c:::::::::::::::::ca:::::aaaa::::::a e::::::::eeeeeeee  s::::::::::::::s a:::::aaaa::::::a r:::::r            
+  cc:::::::::::::::c a::::::::::aa:::a ee:::::::::::::e   s:::::::::::ss   a::::::::::aa:::ar:::::r            
+    cccccccccccccccc  aaaaaaaaaa  aaaa   eeeeeeeeeeeeee    sssssssssss      aaaaaaaaaa  aaaarrrrrrr            
+                                                                                                               
+                      iiii                    hhhhhhh                                                          
+                     i::::i                   h:::::h                                                          
+                      iiii                    h:::::h                                                          
+                                              h:::::h                                                          
+    cccccccccccccccciiiiiiippppp   ppppppppp   h::::h hhhhh           eeeeeeeeeeee    rrrrr   rrrrrrrrr        
+  cc:::::::::::::::ci:::::ip::::ppp:::::::::p  h::::hh:::::hhh      ee::::::::::::ee  r::::rrr:::::::::r       
+ c:::::::::::::::::c i::::ip:::::::::::::::::p h::::::::::::::hh   e::::::eeeee:::::eer:::::::::::::::::r      
+c:::::::cccccc:::::c i::::ipp::::::ppppp::::::ph:::::::hhh::::::h e::::::e     e:::::err::::::rrrrr::::::r     
+c::::::c     ccccccc i::::i p:::::p     p:::::ph::::::h   h::::::he:::::::eeeee::::::e r:::::r     r:::::r     
+c:::::c              i::::i p:::::p     p:::::ph:::::h     h:::::he:::::::::::::::::e  r:::::r     rrrrrrr     
+c:::::c              i::::i p:::::p     p:::::ph:::::h     h:::::he::::::eeeeeeeeeee   r:::::r                 
+c::::::c     ccccccc i::::i p:::::p    p::::::ph:::::h     h:::::he:::::::e            r:::::r                 
+c:::::::cccccc:::::ci::::::ip:::::ppppp:::::::ph:::::h     h:::::he::::::::e           r:::::r                 
+ c:::::::::::::::::ci::::::ip::::::::::::::::p h:::::h     h:::::h e::::::::eeeeeeee   r:::::r                 
+  cc:::::::::::::::ci::::::ip::::::::::::::pp  h:::::h     h:::::h  ee:::::::::::::e   r:::::r                 
+    cccccccccccccccciiiiiiiip::::::pppppppp    hhhhhhh     hhhhhhh    eeeeeeeeeeeeee   rrrrrrr                 
+                            p:::::p                                                                            
+                            p:::::p                                                                            
+                           p:::::::p                                                                           
+                           p:::::::p                                                                           
+                           p:::::::p                                                                           
+                           ppppppppp                                                                           
+                                                                                                               
+"""
